2a49786a3c63d60ca4759baeedf59ab2ddbbef49
Lucas Andrade <Lucasavitorino1@gmail.com>
2024-10-16 09:46:09 -0300

    Updated

--- BackEnd/src/main/java/com/Checkerly/BackEnd/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/Checkerly/BackEnd/config/SecurityConfig.java
@@ -3,7 +3,8 @@ package com.Checkerly.BackEnd.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,14 +12,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-  
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
@@ -28,29 +28,27 @@ public class SecurityConfig {
                         .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .allowCredentials(true); // Se necessário para enviar cookies/autenticação
+                        .allowCredentials(true);
             }
         };
     }
-  
+
     @SuppressWarnings("deprecation")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        .csrf(csrf -> csrf.disable())  // Desabilita CSRF
-        .authorizeRequests(auth -> auth
-            .requestMatchers("/login").permitAll()
-            .requestMatchers("/users/**", "/organizers/**", "/events/**").authenticated()  // Requer autenticação para essas URLs
-            //.anyRequest().permitAll()  // Permite todos os outros acessos
-        )
-        
-        .formLogin(form -> form
-            .loginPage("/login")    
-            .defaultSuccessUrl("/dashboard", true)  // Redireciona para /dashboard após login bem-sucedido
-            .permitAll()  // Permite o acesso à página de login
-        )
-        .logout(logout -> logout.permitAll());
-        
+            .cors(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeRequests(auth -> auth
+                .requestMatchers("/login").permitAll()
+                .requestMatchers("/users/**", "/organizers/**", "/events/**").authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")  //
+                .defaultSuccessUrl("/dashboard", true)
+                .permitAll()
+            )
+            .logout(LogoutConfigurer::permitAll);
         return http.build();
     }
 }
