2e49fe82ae6e98a51a0aadda9991f9bda83a583c
Lucas Andrade <Lucasavitorino1@gmail.com>
2024-10-23 17:22:35 -0300

    Updated

--- BackEnd/src/main/java/com/Checkerly/BackEnd/resources/AuthController.java
+++ BackEnd/src/main/java/com/Checkerly/BackEnd/resources/AuthController.java
@@ -1,39 +1,3 @@
-//package com.Checkerly.BackEnd.resources;
-//
-//import com.Checkerly.BackEnd.domain.Event;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import com.Checkerly.BackEnd.domain.Organizer;
-//import com.Checkerly.BackEnd.domain.User;
-//import com.Checkerly.BackEnd.services.MyUserDetailsService;
-//
-//@RestController
-//@RequestMapping("/auth")
-//public class AuthController {
-//
-//    @Autowired
-//    private MyUserDetailsService userDetailsService;
-//
-//    @PostMapping("/users")
-//    public String registerUser(@RequestBody User user) {
-//        userDetailsService.saveUser(user);
-//        return "User registered successfully!";
-//    }
-//
-//    @PostMapping("/organizers")
-//    public String registerOrganizer(@RequestBody Organizer organizer) {
-//        userDetailsService.saveOrganizer(organizer);
-//        return "Organizer registered successfully!";
-//    }
-//
-//    @PostMapping("/events")
-//    public String registerEvent(@RequestBody Event event) {
-//        userDetailsService.saveEvent(event);
-//        return "Event registered successfully";
-//    }
-//}
-
 package com.Checkerly.BackEnd.resources;
 
 import com.Checkerly.BackEnd.domain.Event;
@@ -61,6 +25,12 @@ public class AuthController {
     public String registerOrganizer(@RequestBody Organizer organizer) {
         userDetailsService.saveOrganizer(organizer);
         return "Organizer registered successfully!";
+    }
+
+    @PostMapping("/events")
+    public String registerEvent(@RequestBody Event event) {
+        userDetailsService.saveEvent(event);
+        return "Event registered successfully";
     }
 
     @GetMapping("/login")
